10a2dc1ee5a3c01f9f9329ac323ed1b912ce5f53
Vikkktorrria <kopylskikh.vm@mail.ru>
2022-06-26 01:00:15 +0500

    добавила метод сравнения eq и подправила добавление данных в публикации

--- server/application/implementation/test/fill_test_data.py
+++ server/application/implementation/test/fill_test_data.py
@@ -19,16 +19,9 @@ def make_envinronment():
 
     dt_now = datetime.datetime.now()
 
-
-    """
     publication_amount = 100
     publications = [get_publication(i, dt_now) for i in range(1, publication_amount + 1)]
-   
-    for publication in publications:
-        obj = Publication(publication['id'], publication['title'], publication['date_of_publication'],
-                          publication['publication_url'],)
-        db.session.add(obj)
-    """
+
     for keyword in keywords:
         obj = KeyWord(keyword['id'], keyword['name'])
         db.session.add(obj)
@@ -40,36 +33,42 @@ def make_envinronment():
     for enterprise in enterprises:
         obj = Enterprise(enterprise['id'], enterprise['name'])
         db.session.add(obj)
+    db.session.commit()
+
+    for publication in publications:
+        obj = Publication(publication['title'], publication['date_of_publication'],
+                          publication['publication_url'], publication['enterprise_id'],
+                          publication['informationResource_id'])
+    db.session.add(obj)
+    db.session.commit()
+
+    for i in range(1, 100):
+        for j in range(1, 5):
+            db.session.execute(
+                f"INSERT INTO public.publication_keyword(publication_id, keyword_id) VALUES ({i}, {j}) on conflict do nothing;")
 
     db.session.commit()
     # сделала сешн коммит, потому что иначе в таблицу publication_keyword ничего не добавится, т.к в keyword ничего нет
-
+    """
     for i in range(1, 100):
         for j in range(1, 5):
             db.session.execute(f'INSERT INTO public.publication(id, title, date_of_publication, '
                                f'publication_url, enterprise_id, "informationResource_id") '
                                f"VALUES ({i}, {i}, '{dt_now}'::date, {i}, {j}, {j}) on conflict do nothing;")
     db.session.commit()
+"""
 
-    for i in range(1, 100):
-        for j in range(1, 5):
-            db.session.execute(f'INSERT INTO public.publication_keyword(publication_id, keyword_id) VALUES ({i}, {j});')
-
-
-    db.session.commit()
 
     return make_response('Записи успешно добавлены', 200)
 
 
 def get_publication(number, date):
     return {
-        'id': number,
         'title': f'publication_{number}',
         'date_of_publication': date,
         'publication_url': f'URL-{number}',
         'enterprise_id': number,
         'informationResource_id': number
-
     }
 
 
--- server/application/models/publicationModel.py
+++ server/application/models/publicationModel.py
@@ -34,11 +34,13 @@ class Publication(db.Model):
 
     informationResource_id = db.Column(db.Integer(), db.ForeignKey('informationresource.id'))
 
-    def __init__(self, public_id, title, date_of_publication, publication_url):
-        self.public_id = public_id
+    def __init__(self, title, date_of_publication, publication_url, enterprise_id, informationResource_id):
         self.title = title
         self.date_of_publication = date_of_publication
         self.publication_url = publication_url
+        self.enterprise_id = enterprise_id
+        self.informationResource_id = informationResource_id
+
 
     def __eq__(self, other):
         return self.title == other.title
@@ -48,7 +50,7 @@ class Publication(db.Model):
 class PublicationSchema(ma.Schema):
     class Meta:
         fields = (
-            'id', 'title', 'date_of_publication', 'publication_url')
+            'title', 'date_of_publication', 'publication_url', 'enterprise_id', 'informationResource_id')
 
 
 # объекты для отправки и приёмов запросов
