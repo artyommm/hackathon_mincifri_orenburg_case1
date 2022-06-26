f22d72053b85a6ac27219646c4dfeade35d3caa1
mautaliev <mautalievsaidamir@gmail.com>
2022-06-26 13:06:13 +0500

    search v2.0

--- server/application/common/templates.py
+++ server/application/common/templates.py
@@ -1,8 +1,15 @@
 SEARCH_PUBLICATIONS = """
     SELECT 
-        * 
+        pb."id",
+        pb."title",
+        to_char(pb."date_of_publication", 'dd-mm-yyyy'),
+        pb."publication_url",
+        en."name",
+        ir."name"
     FROM 
         "publication" pb
+        JOIN "enterprise" en ON pb."enterprise_id" = en."id"
+        JOIN "informationresource" ir ON pb."informationResource_id" = ir."id"
     WHERE 
         pb."id" in (
             SELECT DISTINCT "publication_id" FROM "publication_keyword" pk WHERE pk."keyword_id"=ANY('{keywords}')
